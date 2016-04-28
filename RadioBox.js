0c69e326963282d665562e74dcd8284ab42dff34
Goran Radosevic <goran.radosevic@outlook.com>
2016-04-28 11:47:37 +0200

    Create RadioBox.js
    Created radiobox.js React component

---
+++ RadioBox.js
@@ -0,0 +1,57 @@
+const RadioBox = React.createClass({
+    getInitialState(){
+        return {
+            selected: ''
+        };
+    },
+    render(){
+        const items = this.props.items;
+        const keys = items;
+        if (typeof items === 'object') {
+            keys = Object.keys(items).map(
+                function (key) {
+                    return key;
+                });
+        }
+        return (
+            <table>
+                <tbody>
+                {keys.map(this.displayItem)}
+                </tbody>
+            </table>
+        );
+    },
+    displayItem(value, index){
+        var name = this.props.items[value];
+        var id = this.props.group + '_' + index;
+        var group = this.props.group;
+        return (
+            <tr key={index}>
+                <td>
+                    <input
+                        type="radio"
+                        id={id}
+                        group={group}
+                        name={group}
+                        value={value}
+                        onChange={this.onItemChange}
+                        checked={this.state.selected === value}
+                        />
+                </td>
+                <td>
+                    <label htmlFor={id}>{name}</label>
+                </td>
+            </tr>
+        );
+    },
+    onItemChange(e){
+        const selected = e.target.value;
+        this.setState({selected});
+        this.props.onChange(selected);
+    },
+    componentWillMount(){
+        const selected = this.props.selected;
+        this.setState({selected});
+    }
+});
+export default RadioBox;
